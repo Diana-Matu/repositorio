443cae67d8ee8f7e2a69fd02f99e66ee34465614
Diana Matu <matudiana00@gmail.com>
2023-01-10 17:49:25 -0600

    Archivo que da la suma de un numero entero y uno flotante

---
+++ Suma.java
@@ -0,0 +1,13 @@
+
+/**
+ * Archivo que da la suma de dos numeros, uno entero y uno flotante 
+ */
+//Token: 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
+ public class Suma {
+    public static void main (String args[]){
+        int x=2;
+        double y=2.5;
+        double z=(double)x+y;
+        System.out.print("Primer numero "+x+" Segundo numero "+y+" Resultado "+z);
+    }
+ }  

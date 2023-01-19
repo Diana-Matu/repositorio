75ce8c2d53801d62d34f3f87c1c9f7168e6e9737
Diana Matu <matudiana00@gmail.com>
2023-01-19 15:23:19 -0600

    Archivo que tiene las funciones para dividir enteros y numeros flotantes

---
+++ FuncionesEstaticas/Division.java
@@ -0,0 +1,24 @@
+/**
+ * funcion que hace la division de numeros
+ */
+
+ public class Division {
+   /**
+    * funcion que hace la division de un numero entero
+    * @param x
+    * @return x
+    */
+   public static int division (int x, int y){
+      int result = x/y;
+      return result;
+    }
+    /**
+     * funcion que hace la division de numeros flotantes
+     * @param x
+     * @return x
+     */
+    public static float division (float x, float y){
+      float result = x/y;
+      return result;
+      }
+ }
